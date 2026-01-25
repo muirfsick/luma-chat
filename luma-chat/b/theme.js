7fafb6eaf25af2fa363a9a3707b7142fef23c3d5
dmitry <kitikittttttt@gmail.com>
2026-01-25 18:27:37 +0500

    Delete luma-chat directory

--- luma-chat/b/theme.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector('.theme-toggle');
-const html = document.documentElement;
-
-
-if (!html.dataset.theme) html.dataset.theme = 'light';
-
-toggle.addEventListener('click', () => {
-  html.dataset.theme = html.dataset.theme === 'light' ? 'dark' : 'light';
-});
